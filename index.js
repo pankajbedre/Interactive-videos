baef612ca313c2678331667fc8f42413d2e35723
Pankaj Bedre <pankajbedre22@gmail.com>
2021-10-09 00:01:09 +0530

    optimize

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let onPage = 'Home'
+let onPage = "Home";
 
 $(".play-icon").click(() => {
   if ($(".video").get(0).paused == false) {
@@ -15,30 +15,22 @@ $(".buttons").click(function (e) {
 
   let url = "assets/videos/" + $(this).attr("id") + ".mp4";
   $("#video source").attr("src", url);
-  $("#video")[0].load();
 
   setTimeout(() => {
     $(".video-frame").removeClass("animate");
   }, 1000);
 
   setTimeout(() => {
-    onPage = $(this).attr("id")
-    if(onPage=='Home'){
-      $("#Home").addClass("hidden")
-      $("#Design, #Photography").removeClass("hidden") 
-    } else if(onPage=='Design'){
-      $("#Design").addClass("hidden") 
-      $("#Home, #Photography").removeClass("hidden")
-    } else {
-      $("#Home, #Design").removeClass("hidden") 
-      $("#Photography").addClass("hidden")
-    } 
+    onPage = $(this).attr("id");
+    $("#Home, #Design, #Photography").removeClass("hidden");
+    $("#" + onPage).addClass("hidden");
+    $("#video")[0].load();
   }, 500);
 });
 
-$("#video").mousemove(function() {
-  $("#controls").css("display", "flex")
+$("#video").mousemove(function () {
+  $("#controls").css("display", "flex");
   setTimeout(() => {
-    $("#controls").css("display", "none")
+    $("#controls").css("display", "none");
   }, 2500);
-})
+});
